cb08bc522eec014627c5e5d6b3cee2f238d37382
goomgoompow <flightkjh@gmail.com>
2020-11-12 02:34:13 +0900

    initial commit

---
+++ app/src/main/java/com/goom/timebook/DailyActivity.java
@@ -0,0 +1,20 @@
+package com.goom.timebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DailyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily);
+
+        List<Integer> data = Arrays.asList(1,2,3,4,5,6,7,8);
+//        data.stream().filter()
+}
+}
